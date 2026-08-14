835b5b7d61bdd03c82b073a9657f7c7503594489
Bablu Prasad <babluprasad1448@gmail.com>
2026-08-14 10:15:40 +0530

    Replace SVG logo with image placeholder for logo.png

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -27,13 +27,8 @@ export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 border-b border-[#ebebeb] bg-white">
       <div className="mx-auto flex h-20 max-w-[1760px] items-center justify-between px-6 md:px-10">
-        <Link href="/" className="flex items-center gap-2 text-[var(--airbnb-rausch)]">
-          <svg viewBox="0 0 32 32" className="h-8 w-8 fill-current" aria-hidden="true">
-            <path d="M16 1c2.008 0 3.463.492 4.671 1.354 1.21.861 2.12 2.1 2.946 3.75l.15.31C25.5 10.262 26 12.337 26 14.5c0 3.328-1.127 6.643-3.327 9.809C20.354 27.65 18.232 31 16 31c-2.232 0-4.354-3.35-6.673-6.691C7.127 21.143 6 17.828 6 14.5c0-2.163.5-4.238 2.233-8.086l.15-.31C9.206 4.453 10.117 3.215 11.329 2.354 12.537 1.492 13.992 1 16 1zm0 2c-1.339 0-2.316.33-3.134.912-.816.58-1.503 1.506-2.186 2.871l-.16.337C8.98 10.51 8.5 12.28 8.5 14.5c0 2.846 1.01 5.748 2.99 8.591C13.513 25.992 15.116 29 16 29c.884 0 2.487-3.008 4.51-5.909 1.98-2.843 2.99-5.745 2.99-8.591 0-2.22-.48-3.99-2.02-7.38l-.16-.337C20.638 5.418 19.951 4.492 19.134 3.912 18.316 3.33 17.339 3 16 3zm0 6c2.205 0 4 1.795 4 4 0 2.206-1.795 4-4 4-2.206 0-4-1.794-4-4 0-2.205 1.794-4 4-4zm0 2c-1.103 0-2 .897-2 2 0 1.103.897 2 2 2 1.103 0 2-.897 2-2 0-1.103-.897-2-2-2z"/>
-          </svg>
-          <span className="hidden text-2xl font-bold tracking-tight text-[var(--airbnb-rausch)] xl:block">
-            airbnb
-          </span>
+        <Link href="/" className="flex items-center gap-2">
+          <img src="/logo.png" alt="Airbnb" className="h-8 object-contain" />
         </Link>
 
         {!isHostPage && (
